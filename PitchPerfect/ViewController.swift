af9e5de17e1d30cfbb934cded100d7b1bbb1fbd5
Ed McCormic <edmccormic@gmail.com>
2015-04-23 11:34:20 -0600

    I added microphone button images and created UIButton for the recording and label for recording.

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingInProgress: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton)
+    {
+        //Show text "recording in progress"
+        recordingInProgress.hidden = false
+        
+        //TODO: record the user's voice'
+        println("in Audio")
+        
+    }
 
 }
 
